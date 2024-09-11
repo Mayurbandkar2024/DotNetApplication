2eabbdd6b5acc6ccf58bcb071658f08adbc07c53
Mayurbandkar2024 <mayurbandkar1999@gmail.com>
2024-09-11 11:07:13 +0530

    Created new project

---
+++ WebGnetle/WebGnetle/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace WebGnetle.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ WebGnetle/WebGnetle/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebGnetle.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
