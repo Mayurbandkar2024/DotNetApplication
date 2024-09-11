6becc0d13e722baee7de22827c90096ac5e5a9fd
Mayurbandkar2024 <mayurbandkar1999@gmail.com>
2024-09-11 12:04:03 +0530

    add new class

---
+++ WebGnetle/WebGnetle/Models/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebGnetle.Models
+{
+    public class Class1
+    {
+        public int name { get; set; }
+    }
+}
